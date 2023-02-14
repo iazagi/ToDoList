a0a6fcfd2514da1b4ff3860b4401b373a3663b20
iazagi <101048623+iazagi@users.noreply.github.com>
2023-02-14 11:01:49 +0200

    the end

--- server/db.js
+++ server/db.js
@@ -1,11 +1,10 @@
 const Pool = require('pg').Pool;
-//import * as dotenv from 'dotenv';
 require('dotenv').config();
 
 const pool = new Pool({
-  user: 'postgres', //process.env.USERNAME
-  password: 'Pazagi1704', //process.env.PASSWORD,
-  host: 'localhost', //process.env.HOST,
+  user: 'postgres', //process.env.USERNAME,
+  password: 'Pazagi1704#', // process.env.PASSWORD,
+  host: 'http://localhost/', //process.env.HOST,
   port: '5432', //process.env.DBPORT,
   database: 'todolist',
 });
--- server/server.js
+++ server/server.js
@@ -1,18 +1,32 @@
-const PORT = process.env.PORT ?? 8000;
+const PORT = 8000;
 const express = require('express');
-const { v4: uuidv4 } = require('uuid');
-const pool = require('./db');
 const cors = require('cors');
-
 const app = express();
+const pool = require('./db');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-//app.get('/', (req, res) => {});
 app.use(cors());
 app.use(express.json());
-//GET ALL TODOS
+
+// add new todo
+app.post('/todos', async (req, res) => {
+  try {
+    const { user_email, title, progress, date } = req.body;
+    console.log(req.body);
+    const newToDo = await pool.query(
+      `INSERT INTO todos (user_email, title, progress, date) VALUES($1, $2, $3, $4)`,
+      [user_email, title, progress, date]
+    );
+    res.json(newToDo);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// get all todos for user by userEmail
 app.get('/todos/:userEmail', async (req, res) => {
   const { userEmail } = req.params;
-
   try {
     const todos = await pool.query(
       'SELECT * FROM todos WHERE user_email = $1',
@@ -20,57 +34,94 @@ app.get('/todos/:userEmail', async (req, res) => {
     );
     res.json(todos.rows);
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 });
 
-// CREATE NEW TODOS
-
-app.post('/todos', async (req, res) => {
-  const { user_email, title, progress, date } = req.body;
-  console.log(user_email, title, progress, date);
-  const id = uuidv4();
+// delete a todo
+app.delete('/todos/:id', async (req, res) => {
+  const { id } = req.params;
   try {
-    const newTodo = await pool.query(
-      `INSERT INTO todos(id, user_email, title, progress, date) VALUES($1, $2, $3, $4, $5)`,
-      [id, user_email, title, pprogress, date]
-    );
-    res.json(newTodo);
-  } catch (err) {
-    console.error(err);
+    const deleteToDo = await pool.query('DELETE FROM todos WHERE id = $1;', [
+      id,
+    ]);
+    res.json(deleteToDo);
+  } catch (error) {
+    console.error(error);
   }
 });
 
-// EDIT TODOS
-
+// edit a todo
 app.put('/todos/:id', async (req, res) => {
   const { id } = req.params;
   const { user_email, title, progress, date } = req.body;
-
   try {
-    await pool.query(
-      '/Update todos SET user_email = $1, title = $2, progress = $3, date = $4 WHERE id = $5;',
+    const editToDo = await pool.query(
+      'UPDATE todos SET user_email =$1, title = $2, progress = $3, date = $4 WHERE id = $5;',
       [user_email, title, progress, date, id]
     );
-    res.json(editTodo);
+    res.json(editToDo);
   } catch (error) {
     console.error(error);
   }
 });
 
-// DELETE A TODO
-
-app.delete('/todos/:id', async (req, res) => {
-  const { id } = req.params;
-
+//sign up
+app.post('/signup', async (req, res) => {
   try {
-    const deleteTodo = await pool.query('DELETE FROM todos WHEREid = $1;', [
-      id,
+    const { email, password } = req.body;
+
+    console.log('PASSWORD', password);
+
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    const signUp = await pool.query(
+      `INSERT INTO users (email, hashed_password) VALUES($1, $2)`,
+      [email, hashedPassword]
+    );
+
+    const token = jwt.sign({ email }, 'secret', { expiresIn: '1h' });
+
+    res.json({ email, token });
+    const error = signUp.name === 'error';
+
+    if (!error) {
+      res.json({ email, token });
+    } else {
+      res.json({ detail: signUp.detail });
+    }
+  } catch (error) {
+    res.json(error);
+    console.error(error);
+  }
+});
+
+//login
+app.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const users = await pool.query('SELECT * FROM users WHERE email = $1;', [
+      email,
     ]);
-    res.json(deleteTodo);
+    console.log(users);
+
+    if (!users.rows.length) return res.json({ detail: 'User does not exist' });
+
+    const success = await bcrypt.compare(
+      password,
+      users.rows[0].hashed_password
+    );
+    const token = jwt.sign({ email }, 'secret', { expiresIn: '1h' });
+
+    if (success) {
+      res.json({ email: users.rows[0].email, token });
+    } else {
+      res.json({ detail: 'Login failed' });
+    }
   } catch (error) {
     console.error(error);
   }
 });
 
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
+app.listen(PORT, () => `Server running on PORT ${PORT}`);
